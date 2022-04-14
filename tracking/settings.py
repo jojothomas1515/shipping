1e89cde589f39ec27bc178dbfbd967cedf8ef4eb
Jojo Thomas <jojothomas1515@gmail.com>
2022-04-14 01:51:18 +0100

    make it better

--- tracking/settings.py
+++ tracking/settings.py
@@ -1,5 +1,6 @@
 import os.path
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+=6cdc043-89xxbv(e&ncd3(8mbmc76%#(v@$fa1wn)((+6ef!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -107,3 +108,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
